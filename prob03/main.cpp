fcff2a1f8987b3d368dd2677d9e520d4ebdd7796
cortes28 <cortes28@csu.fullerton.edu>
2019-09-14 14:36:21 -0700

    made a formula for the projected sales for the next year

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -6,11 +6,11 @@ int main ()
 
 {
 
-long int year_projection_sales;
-const double PERCENTAGE_OF_INCREASED_SALES = 1.18;
-const long int LAST_YEAR_SALES = 2103419277;
+  long int year_projection_sales;
+  const double PERCENTAGE_OF_INCREASED_SALES = 1.18;
+  const long int LAST_YEAR_SALES = 2103419277;
 
-std::cout << "Last year\'s sales were $" << LAST_YEAR_SALES << std::endl;
+  std::cout << "Last year\'s sales were $" << LAST_YEAR_SALES << std::endl;
 
 
   year_projection_sales = LAST_YEAR_SALES * PERCENTAGE_OF_INCREASED_SALES;
@@ -19,6 +19,6 @@ std::cout << "Last year\'s sales were $" << LAST_YEAR_SALES << std::endl;
 
 
 
-return 0;
+  return 0;
 
 }
