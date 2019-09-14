273d9b3c3ead8ada5af823782daa80c16767c19c
cortes28 <cortes28@csu.fullerton.edu>
2019-09-14 14:02:59 -0700

    made a formula for the projected sales for next year using the information from last year

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,24 @@
 // Sales prediction
+
+#include <iostream>
+
+int main ()
+
+{
+
+long int year_projection_sales;
+const double PERCENTAGE_OF_INCREASED_SALES = 1.18;
+const long int LAST_YEAR_SALES = 2103419277;
+
+std::cout << "Last year\'s sales were $" << LAST_YEAR_SALES << std::endl;
+
+
+  year_projection_sales = LAST_YEAR_SALES * PERCENTAGE_OF_INCREASED_SALES;
+
+  std::cout << "This year\'s sales prediction: $" << year_projection_sales <<std::endl;
+
+
+
+return 0;
+
+}
